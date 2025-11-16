47d7f62e5f8d06d902917b122fe9294cfdc88fbb
Maria Gonçalves <mariaparecidamp99@hotmail.com>
2025-11-16 19:59:04 +0000

    atualização

--- js/main.js
+++ js/main.js
@@ -19,43 +19,5 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
-  // ======== SPA (Single Page Application) ========
-  const conteudo = document.getElementById("conteudo");
-
-  function carregarPagina(pagina) {
-    fetch(`../pages/${pagina}.html`)
-      .then((res) => {
-        if (!res.ok) throw new Error("Página não encontrada");
-        return res.text();
-      })
-      .then((html) => {
-        conteudo.innerHTML = html;
-
-       
-        if (pagina === "cadastro") {
-          inicializarFormulario();
-        } else if (pagina === "login") {
-          inicializarLogin();
-        }
-      })
-      .catch(() => {
-       conteudo.innerHTML = `<h2 style="text-align:center;color:red;">Erro ao carregar a página "${pagina}".</h2>`;
-console.error("Erro ao carregar:", pagina);
-      });
-  }
-
-function inicializarLogin() {
-  console.log("Página de login carregada");
-}  
-
-function inicializarFormulario() {
-  console.log("Página de cadastro carregada");
-}
-  window.carregarPagina = carregarPagina;
-
- 
-  carregarPagina("home");
-
-  
-  carregarUsuario();
-});
+      carregarUsuario();
+    });
